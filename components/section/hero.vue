754589f4af3c32a1114e5452ec04dbad1a197b1c
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-28 15:31:02 +0200

    sizing

--- components/section/hero.vue
+++ components/section/hero.vue
@@ -1,11 +1,11 @@
 <template>
-	<div class="relative bg-dark pb-16">
+	<div class="relative bg-dark pb-20">
 		<div class="wrapper">
 			<img class="mx-auto" src="/img/romi.png" alt="Bring Romi home" />
 		</div>
 		<div class="absolute bottom-0 left-0 w-full bg-dark/50 py-8 text-center">
-			<h2 class="text-4xl font-bold text-white">רומי</h2>
-			<p class="text-lg font-medium text-gray-300">מחכה שנחזיר אותה הביתה</p>
+			<h2 class="text-5xl font-bold text-white">רומי</h2>
+			<p class="text-2xl font-medium text-gray-300">מחכה שנחזיר אותה הביתה</p>
 			<HandBtn class="translate-y-16">גם אני רוצה לתת יד</HandBtn>
 		</div>
 	</div>
