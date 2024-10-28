a3ee7859f1a783bb50b7b6a66bd5a743f69dc535
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-28 18:34:06 +0200

    Create second section

---
+++ components/section/Mahut.vue
@@ -0,0 +1,31 @@
+<template>
+	<section class="py-24">
+		<div class="wrapper text-center">
+			<h2 class="mb-12 text-5xl font-bold">מהות הפרוייקט</h2>
+			<ul class="grid grid-cols-3 justify-items-center">
+				<figure class="text-mid max-w-48">
+					<img class="mx-auto mb-4" src="/img/hand-in-heart.svg" alt="Hand in heart" />
+					<figcaption class="text-xl font-semibold">כל אחד יכול לתמוך מכל מקום</figcaption>
+				</figure>
+
+				<figure class="text-mid max-w-48">
+					<img class="mx-auto mb-4" src="/img/hands-in-circle.svg" alt="Hands in circle" />
+					<figcaption class="text-xl font-semibold">נחזור להיות שלמים רק כשכולם חוזרים</figcaption>
+				</figure>
+
+				<figure class="text-mid max-w-48">
+					<img class="mx-auto mb-4" src="/img/hands-in-triangle.svg" alt="Hands in triangle shape" />
+					<figcaption class="text-xl font-semibold">כולנו יחד נחזיר אותם הביתה</figcaption>
+				</figure>
+			</ul>
+
+			<div>
+				<h5>This is some underlined text</h5>
+			</div>
+		</div>
+	</section>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="sass" scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,6 @@
 <template>
 	<SectionHero />
+	<SectionMahut />
 	<div style="height: 3000px"></div>
 </template>
 
