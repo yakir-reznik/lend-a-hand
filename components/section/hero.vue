c02e940cdd2cd66a24f2f472249c182093c7ec2e
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-28 18:33:51 +0200

    styled underline

--- components/section/hero.vue
+++ components/section/hero.vue
@@ -11,10 +11,20 @@
 				<div class="absolute bottom-0 left-0 text-center">
 					<p class="text-6xl font-black text-primary">124,345</p>
 					<h5 class="text-xl font-semibold tracking-wide text-white">נתנו יד להשבת החטופים</h5>
+					<img
+						class="absolute -bottom-1 left-0 h-[6px]"
+						src="/img/underline.svg"
+						alt="Yellow styled underline"
+					/>
 				</div>
 				<div class="right-0-0 absolute bottom-0 text-center">
 					<p class="text-6xl font-black text-primary">387</p>
 					<h5 class="text-xl font-semibold tracking-wide text-white">ימים בשבי</h5>
+					<img
+						class="absolute -bottom-1 left-0 h-[6px]"
+						src="/img/underline.svg"
+						alt="Yellow styled underline"
+					/>
 				</div>
 			</div>
 		</div>
