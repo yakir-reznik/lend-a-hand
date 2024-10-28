ded1dde861ad1cf6ce634f44d6072b8cef8feeb7
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-28 15:27:23 +0200

    add nuxt fonts and floating-icons

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	compatibilityDate: '2024-04-03',
-	modules: ['@nuxtjs/tailwindcss'],
+	modules: ['@nuxtjs/tailwindcss', '@nuxt/fonts'],
 	app: {
 		head: {
 			htmlAttrs: { lang: 'he', dir: 'rtl' },
