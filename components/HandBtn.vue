1f733f0007cad6e5a487b71d771934d30045dc6f
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-28 15:27:52 +0200

    Create button component

---
+++ components/HandBtn.vue
@@ -0,0 +1,50 @@
+<template>
+	<button
+		class="hand-btn inline-flex items-center gap-4 rounded-full bg-primary px-12 py-4 text-xl font-bold shadow-dark transition hover:bg-primary-400 active:scale-[0.975]"
+		@click="handleClick"
+	>
+		<img src="/img/one-hand.svg" alt="One hand" />
+		<span><slot></slot></span>
+	</button>
+</template>
+
+<script setup lang="ts">
+	import { useFloatingIcons } from 'floating-icons'
+
+	const handleClick = (e: MouseEvent) => {
+		useFloatingIcons({
+			target: e.currentTarget as HTMLElement,
+			minSize: '40px',
+			maxSize: '60px',
+			density: 0.625,
+			elements: [
+				{
+					content: '👋',
+					probability: 4,
+				},
+				{
+					content: '✋',
+					probability: 4,
+				},
+				{
+					content: '❤️',
+					probability: 1,
+				},
+				{
+					content: '🇮🇱',
+					probability: 1,
+				},
+			],
+		})
+	}
+</script>
+
+<style lang="css" scoped>
+	.hand-btn {
+		box-shadow: 0 6px 0 0 var(--tw-shadow-color);
+		transition: box-shadow 100ms;
+	}
+	.hand-btn:active {
+		box-shadow: 0 3px 0 0 var(--tw-shadow-color);
+	}
+</style>
