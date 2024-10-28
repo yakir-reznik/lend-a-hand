cba3d7f0c32c43e4d9df01ad071d6c611015f610
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-28 11:17:43 +0200

    add debug component

---
+++ components/Debug.vue
@@ -0,0 +1,7 @@
+<template>
+	<pre dir="ltr" class="bg-gray-100 p-4 text-left text-sm text-gray-900"><slot></slot></pre>
+</template>
+
+<script setup lang="ts"></script>
+
+<style lang="sass" scoped></style>
