74a3209a9983fa1bb75850046f17d59a94466ad6
Yakir Reznik <yakir.reznik@gmail.com>
2024-10-29 17:08:19 +0200

    form

--- components/section/Signup.vue
+++ components/section/Signup.vue
@@ -6,19 +6,26 @@
 			</h2>
 			<form @submit="handleSubmit">
 				<fieldset class="flex justify-center gap-8">
-					<button class="grid place-items-center transition active:scale-95" @click="data.under12 = true">
+					<button
+						class="group grid place-items-center transition active:scale-95"
+						:class="{ selected: data.under12 }"
+						@click="data.under12 = true"
+					>
+						<!-- bg-primary-100 -->
 						<div
-							class="mb-2 grid size-20 place-items-center rounded-full border-2 bg-white p-4 transition"
-							:class="{ 'border-primary-300 bg-primary-100': data.under12 }"
+							class="mb-2 grid size-20 place-items-center rounded-full border-2 bg-white p-4 transition group-[.selected]:border-primary-400 group-[.selected]:bg-primary-100"
 						>
 							<img class="w-6" src="/img/abstract-yellow-hand.svg" alt="hand icon" />
 						</div>
 						<label class="text-lg font-semibold">אני מתחת לגיל 12</label>
 					</button>
-					<button class="grid place-items-center transition active:scale-95" @click="data.under12 = false">
+					<button
+						class="group grid place-items-center transition active:scale-95"
+						:class="{ selected: !data.under12 }"
+						@click="data.under12 = false"
+					>
 						<div
-							class="mb-2 grid size-20 place-items-center rounded-full border-2 bg-white p-4 transition"
-							:class="{ 'border-primary-400 bg-primary-100': !data.under12 }"
+							class="mb-2 grid size-20 place-items-center rounded-full border-2 bg-white p-4 transition group-[.selected]:border-primary-400 group-[.selected]:bg-primary-100"
 						>
 							<img class="w-10" src="/img/abstract-yellow-hand.svg" alt="hand icon" />
 						</div>
@@ -43,11 +50,28 @@
 						v-model="data.email"
 					/>
 				</fieldset>
+				<fieldset class="flex flex-wrap gap-2">
+					<label class="mb-4 mt-8 block w-full text-center text-lg font-semibold">אפשר גם להוסיף ברכה:</label>
+					<div
+						v-for="(message, messageId) in messages"
+						:key="message"
+						class="group"
+						:class="{ selected: data.messageId === messageId }"
+					>
+						<button
+							@click="data.messageId = messageId"
+							class="text-dark-800/60 group-[.selected]:text-dark-800 rounded-md border border-b-4 border-r-4 border-gray-300 bg-[#EFE9D8] px-4 py-2 text-lg font-semibold transition active:scale-95 group-[.selected]:border-primary-600/60 group-[.selected]:bg-primary"
+						>
+							{{ message }}
+						</button>
+					</div>
+				</fieldset>
 				<div class="mt-12 h-32 text-center">
 					<HandBtn class="mx-auto w-5/6">צרפו אותי</HandBtn>
 				</div>
 			</form>
 		</div>
+		<Debug>{{ data }}</Debug>
 	</section>
 </template>
 
@@ -56,10 +80,24 @@
 		e.preventDefault()
 	}
 
-	const data = reactive({
+	const messages = {
+		gave_hand: 'גם אני נתתי יד',
+		miss_you: 'מתגעגעים אליכם',
+		want_you_home: 'גם אני רוצה אתכם בבית',
+		it_will_be_good: 'כמה טוב יהיה כשתשובו',
+	}
+
+	type Data = {
+		under12: boolean
+		name: string
+		email: string
+		messageId: keyof typeof messages
+	}
+	const data = reactive<Data>({
 		under12: false,
 		name: '',
 		email: '',
+		messageId: 'gave_hand',
 	})
 </script>
 
