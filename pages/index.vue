5014b326866ec6331329f59bd0b087f50c17bc41
Yakir Reznik <yakir.reznik@gmail.com>
2024-12-23 16:16:32 +0200

    auto refresh via query param

--- pages/index.vue
+++ pages/index.vue
@@ -11,6 +11,17 @@
 	// fetch data from server
 	const userStore = useUserStore()
 	userStore.load()
+
+	// check if url has query params "auto-refresh"
+	onMounted(() => {
+		const autoRefresh = useRoute().query['auto-refresh'] === 'true'
+		if (!autoRefresh) return
+
+		// Reload after 1 minute
+		setTimeout(() => {
+			window.location.reload()
+		}, 180000)
+	})
 </script>
 
 <style lang="sass" scoped></style>
