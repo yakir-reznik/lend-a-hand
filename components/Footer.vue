73d72e3877e09d32fd6824b731569640c1cbe7e6
Yakir Reznik <yakir.reznik@gmail.com>
2025-02-01 18:39:05 +0200

    add email address to footer

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,12 +1,27 @@
 <template>
 	<footer class="bg-dark-900 py-8 desktop:py-2">
 		<div class="wrapper">
+			<div class="pb-3 desktop:pt-6">
+				<p class="text-center text-white/60">
+					<span>ליצירת קשר:</span>
+					<a
+						class="inline-block px-2 hover:text-white/80 hover:underline"
+						href="mailto:info@notnim-yad.net"
+						title="צרו קשר עם הנהלת המיזם"
+					>
+						info@notnim-yad.net
+					</a>
+				</p>
+			</div>
+
+			<div class="mx-auto mb-5 mt-3 h-[2px] w-16 rounded bg-white/20" />
+
 			<ul class="flex flex-col justify-center gap-2 desktop:flex-row">
 				<li>
 					<a
 						href="https://www.linkedin.com/in/iris-duani-3377b618/"
 						target="_blank"
-						class="text-white/60 hover:text-white/80"
+						class="block text-center text-white/60 hover:text-white/80"
 					>
 						עיצוב וחווית משתמש: איריס דואני
 					</a>
@@ -16,7 +31,7 @@
 					<a
 						href="https://www.thereznik.com/?utm_source=notnim-yad&utm_medium=referral&utm_campaign=footer-link"
 						target="_blank"
-						class="text-white/60 hover:text-white/80"
+						class="block text-center text-white/60 hover:text-white/80"
 					>
 						בניית אתר ואנימציה: יקיר רזניק
 					</a>
@@ -26,7 +41,7 @@
 					<a
 						href="https://www.adir.me/?utm_source=notnim-yad&utm_medium=referral&utm_campaign=footer-link"
 						target="_blank"
-						class="text-white/60 hover:text-white/80"
+						class="block text-center text-white/60 hover:text-white/80"
 					>
 						Adir Cohen - Product Design & Fullstack
 					</a>
