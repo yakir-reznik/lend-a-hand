6e6404f088c3b3fc664e73f4e1ce10cff25350d2
Yakir Reznik <yakir.reznik@gmail.com>
2025-02-03 13:48:45 +0200

    we don't need to outer object....

--- pages/activity/[type].vue
+++ pages/activity/[type].vue
@@ -6,34 +6,28 @@
 	<template v-else>
 		<!-- <pre style="padding-top: 100px" dir="ltr">{{ contentfulData.fields }}</pre> -->
 		<ActivityTopSection
-			:image="contentfulData?.fields?.mainImage?.fields?.file?.url"
-			:subtitle="contentfulData?.fields?.subtitle"
-			:title="contentfulData?.fields?.title"
-			:developedBy="contentfulData?.fields?.developed_by"
-			:about="contentfulData?.fields?.about"
+			:image="contentfulData?.mainImage?.fields?.file?.url"
+			:subtitle="contentfulData?.subtitle"
+			:title="contentfulData?.title"
+			:developedBy="contentfulData?.developed_by"
+			:about="contentfulData?.about"
 		/>
-		<Preparation :preparations="contentfulData?.fields?.preperation" />
+		<Preparation :preparations="contentfulData?.preperation" />
 		<ActivityOpeningTalk
-			:sections="contentfulData?.fields?.openingTalk"
-			:image="contentfulData?.fields?.openingTalkImage?.fields?.file?.url"
+			:sections="contentfulData?.openingTalk"
+			:image="contentfulData?.openingTalkImage?.fields?.file?.url"
 		/>
-		<ActivityFlow
-			:sections="contentfulData?.fields?.flow"
-			:image="contentfulData?.fields?.flowImage?.fields?.file?.url"
-		/>
-		<ActivitySummary
-			:sections="contentfulData?.fields?.summary"
-			:image="contentfulData?.fields?.summaryImage?.fields?.file?.url"
-		/>
-		<ActivityGuidelines :sections="contentfulData?.fields?.guideline" />
-		<ActivityGallery :galleryItems="activityData.galleryItems" />
+		<ActivityFlow :sections="contentfulData?.flow" :image="contentfulData?.flowImage?.fields?.file?.url" />
+		<ActivitySummary :sections="contentfulData?.summary" :image="contentfulData?.summaryImage?.fields?.file?.url" />
+		<ActivityGuidelines :sections="contentfulData?.guideline" />
+		<!-- <ActivityGallery :galleryItems="activityData.galleryItems" /> -->
 		<p class="mt-12 text-center text-3xl font-bold">מערכי פעילות נוספים</p>
 		<ActivitiesGrid />
 	</template>
 </template>
 
 <script setup lang="ts">
-	import { createClient, type Entry, type EntrySkeletonType } from 'contentful'
+	import { createClient } from 'contentful'
 
 	// Add Contentful client configuration
 	// Remove this and move to ENV
@@ -43,7 +37,9 @@
 	})
 
 	// Add new ref for Contentful data
-	const contentfulData = ref<Entry<EntrySkeletonType, undefined, string>>()
+	// There's a way to get this properly typed using contentful-cli
+	// I don't know if its revelant for this project
+	const contentfulData = ref<any>()
 
 	const route = useRoute()
 	const type = route.params.type.toString()
@@ -52,15 +48,23 @@
 	const isLoading = ref(true)
 
 	onMounted(async () => {
-		isLoading.value = true
+		// Navigate to activity index page if type is not found
+		if (!activitiesTypes.includes(type)) {
+			navigateTo('/activity-plans')
+			throw createError({
+				statusCode: 404,
+				message: 'Activity not found',
+			})
+		}
+
 		try {
 			const response = await client.getEntries({
 				content_type: 'activity',
 				'fields.type': route.query.type,
 			})
 
-			if (response.items.length > 0) {
-				contentfulData.value = response.items[0]
+			if (response.items.length) {
+				contentfulData.value = response.items[0].fields
 			}
 		} catch (error) {
 			console.error('Error fetching data from Contentful:', error)
@@ -70,22 +74,4 @@
 	})
 
 	const activitiesTypes = ['stones', 'photos', 'prints', 'outline', 'symbols']
-
-	// Update computed property to merge static and Contentful data
-	const activityData = computed(() => {
-		// Navigate to activity index page if type is not found
-		if (!activitiesTypes.includes(type)) {
-			navigateTo('/activity-plans')
-			throw createError({
-				statusCode: 404,
-				message: 'Activity not found',
-			})
-		}
-		if (contentfulData.value) {
-			return {
-				contentful: contentfulData.value.fields,
-			}
-		}
-		return staticData
-	})
 </script>
