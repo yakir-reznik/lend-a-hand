04599b99570e52d89afb00f1be99785308d0f635
Nir Tzezana <nir@appcharge.com>
2025-02-09 19:06:09 +0200

    updates

--- pages/appendices.vue
+++ pages/appendices.vue
@@ -39,11 +39,29 @@
 									{{ contentfulData?.items[0].fields.handSubTitle }}
 								</h3>
 								<p class="mt-5 text-right text-gray-700">
-									{{ contentfulData?.items[0].fields.handDescription }}
+									<div class="text-right px-4 py-8">
+  <p class="text-lg mb-6">
+    כף היד היא שפת הקשר האנושי, המגע האוהב והבלתי אמצעי מרגע הלידה, הקשר עם ההורים ועם החברים, לחיצת היד לשלום ולהסכמה.
+  </p>
+
+  <ul class="space-y-4 list-inside list-disc">
+    <li>
+      כף היד מתכתבת עם ה"לייק" אותו אנו רגילים לסמן כאות לאהדה ברשתות חברתיות <span class="text-2xl">👍</span>
+    </li>
+
+    <li>
+      כף היד יכולה לשמש "קוביית לגו" למבנים ולרעיונות (השמיים הם הגבול...)
+    </li>
+
+    <li>
+      כף היד מאפשרת יצירה של פרויקטים רבים - באופן דיגיטלי, פיזי ובמרחב הציבורי
+    </li>
+  </ul>
+</div>
 								</p>
 							</div>
 							<div class="flex flex-col items-center justify-center">
-								<img src="/img/appendcies/hands.png" alt="Hands" class="w-full" />
+								<img src="/img/appendcies/hands.png" alt="Hands" class="h-[260px] w-[380px] object-cover" />
 							</div>
 						</div>
 					</div>
@@ -58,7 +76,18 @@
 									{{ contentfulData?.items[0].fields.mutualResponsibility }}
 								</h2>
 								<p class="mt-5 text-right text-gray-700">
-									{{ contentfulData?.items[0].fields.mutualResponsibilityDescription }}
+									הכרה באחריות והבעת תמיכה מצד הקהילה כלפי כל אחד מחבריה, ומצד כל אחד מחברי הקהילה כלפי הכלל.<br>
+
+הערבות ההדדית נבחנת ביחס לחלשים בחברה: הזקנים, החולים, העניים והנזקקים כאשר הם, והיא ערובה לחוסנה של החברה.<br>
+
+התחושה של ערבות הדדית מגבירה שייכות ומצמצמת ניכור.<br>
+
+ערבות הדדית וסולידריות חברתית תורמות לצמצום אלימות.<br>
+
+ערבות הדדית היא ערך מורכב המכיל תפיסות ורגשות כמו אמפתיה, חמלה, יחס לכבוד האדם, הזדהות, קרבה, חברות ואהבת אדם.<br>
+הסולידריות היא ערך אוניברסלי שאותו רואות בחיוב דתות ואמונות שונות (משרד החינוך 2020).<br><br>
+
+* ראו בנספח מהי ערבות הדדית לאור פתגמים ממקורות של דתות שונות.
 								</p>
 							</div>
 						</div>
@@ -68,21 +97,21 @@
 		</div>
 		<div class="-mt-14 px-4 py-10 pb-10 md:px-10">
 			<div class="mx-auto max-w-6xl text-center">
-				<h2 class="mb-8 text-2xl font-bold text-gray-900 md:text-3xl">המיזם מאפשר נתינה בדרך חדשה</h2>
+				<h2 class="mb-8 text-2xl font-bold text-gray-900 md:text-3xl">מערכי-שיעור על ערבות הדדית</h2>
 				<div class="grid grid-cols-1 items-start md:grid-cols-1">
 					<div class="grid grid-cols-1 gap-8 text-right md:grid-cols-3">
-						<div v-for="(preparation, index) in preparations" :key="index">
+						<div v-for="(classRoomActivity, index) in classRoomActivities" :key="index">
 							<div>
 								<div
 									class="flex h-8 min-h-8 w-8 min-w-8 items-center justify-center rounded-full bg-yellow-300 text-center text-lg font-bold"
 								>
 									{{ index + 1 }}
 								</div>
-								<h3 class="mt-4 text-right text-lg font-bold text-gray-900 md:text-2xl">
-									{{ preparation.fields.title }}
-								</h3>
+								<a :href=classRoomActivity.url><h3 class="underline mt-4 text-right text-lg font-bold text-gray-900 md:text-2xl">
+									{{ classRoomActivity.title }}
+								</h3></a>
 								<div class="mt-2 list-inside list-disc text-right leading-relaxed text-gray-700">
-									{{ preparation.fields.descrpition }}
+									{{ classRoomActivity.description }}
 								</div>
 							</div>
 						</div>
@@ -95,13 +124,13 @@
 				<h2 class="mb-2 text-2xl font-bold text-gray-900 md:text-3xl">המיזם מאפשר נתינה בדרך חדשה</h2>
 				<div class="grid grid-cols-1 items-start md:grid-cols-1">
 					<div class="grid grid-cols-1 gap-8 text-right md:grid-cols-3">
-						<div v-for="(preparation, index) in preparations" :key="index">
+						<div v-for="(preparation, index) in mutualResponsibilityValue" :key="index">
 							<div>
 								<h3 class="mt-4 text-right text-lg font-bold text-gray-900 md:text-2xl">
-									{{ preparation.fields.title }}
+									{{ preparation.title }}
 								</h3>
 								<div class="mt-2 list-inside list-disc text-right leading-relaxed text-gray-700">
-									{{ preparation.fields.descrpition }}
+									{{ preparation.descrpition }}
 								</div>
 							</div>
 						</div>
@@ -379,6 +408,31 @@
 		'ספרים ומסך מחשב',
 		'ספרים ומסך מחשב',
 		// Add more items here
+	]
+
+	const mutualResponsibilityValue = [
+		{
+			title: "בדת הדרוזית",
+			descrpition: 'حفظ اﻻخوان خفد الأخوان، "لسمر عليهم" - عرض الموجه لمجموعات مختلفة. شيطان قسم بالدر: "المسؤولية والمسؤولية لأخيك البيولوجي، لبن عمك، لأخيك الإنساني في كل مكان". من عطف الاقتباس',
+		},
+	]
+
+	const classRoomActivities = [
+		{
+				title: 'ערכה לערבות הדדית, משרד החינוך תש"פ',
+				url: '',
+				description: 'להקשיב, לשמוע, לראות ולהשפיע ערכה לאנשי החינוך, תש"ף',
+		},
+		{
+			title: 'אוגדן פעילויות (2020) מנהל חברה ונוער',
+			url: '',
+			description: 'מגוון נושאים: ערבות הדדית בקבוצה החברתית, שוויון הזדמנויות, הצלת חיים, יזמות תפוצות ועוד.',
+		},
+		{
+			title: 'אוגדן פעילויות (2020) מנהל חברה ונוער',
+			url: '',
+			description: 'מגוון נושאים: ערבות הדדית בקבוצה החברתית, שוויון הזדמנויות, הצלת חיים, יזמות תפוצות ועוד.',
+		},
 	]
 
 	const preparations = [
