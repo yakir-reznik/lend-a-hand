2e9b20c6a4f6bac1cbe36b2a41ab399b2dd08540
almog kashany <almogk012@gmail.com>
2025-02-09 09:37:12 +0200

    rm console

--- pages/activity/[type].vue
+++ pages/activity/[type].vue
@@ -65,7 +65,6 @@
 
 			if (response.items.length) {
 				contentfulData.value = response.items.find((item) => item.fields.type == type).fields
-				console.log('Contentful data:', contentfulData.value)
 			}
 		} catch (error) {
 			console.error('Error fetching data from Contentful:', error)
